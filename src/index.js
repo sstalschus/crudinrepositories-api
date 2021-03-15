22e89253d6ef4aa3b20be4a2d24b2288889f094e
Samuel Stalschus <70023098+SamStalschus@users.noreply.github.com>
2021-03-15 16:57:49 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json('likes')
 });
 
 module.exports = app;
